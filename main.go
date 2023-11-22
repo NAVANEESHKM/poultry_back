b0c3e9d849031396af72cae541dc5b76bbeda4c9
NAVANEESH K M <navaneesh.ee20@bitsathy.ac.in>
2023-11-22 10:21:43 +0530

    first commit

--- main.go
+++ main.go
@@ -48,7 +48,7 @@ func initApp(mongoClient *mongo.Client){
 func main(){
 	server = gin.Default()
 	server.Use(cors.New(cors.Config{
-		AllowOrigins: []string{"https://poultry-front.vercel.app/"},
+		AllowOrigins: []string{"http://localhost:3000/"},
 		AllowMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders: []string{"Origin", "Content-Type"},
 	  }))
