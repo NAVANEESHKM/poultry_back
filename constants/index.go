f9ee07ac1acb4f75076bf93b80be4ca6991ec561
NAVANEESH K M <85608508+NAVANEESHKM@users.noreply.github.com>
2024-02-27 12:02:15 +0530

    Update index.go

--- constants/index.go
+++ constants/index.go
@@ -2,7 +2,7 @@
 
 package constants
 
-const ConnectionString ="mongodb+srv://user:pass@cluster0.mom8ir1.mongodb.net/"
+const ConnectionString ="mongodb+srv://user:pass@cluster0.mom8ir1.mongodb./"
 //
 //mongodb+srv://navaneeshmuthusamy:Neesh04%4012%402002@cluster0.j5hnmx6.mongodb.net/??retryWrites=true&w=majority
 const Port =":4000"
