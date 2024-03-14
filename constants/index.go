47919703a0e2a900fd53f2744a7f694d835d1663
NAVANEESH K M <85608508+NAVANEESHKM@users.noreply.github.com>
2024-03-14 17:11:46 +0530

    Update index.go
    kk

--- constants/index.go
+++ constants/index.go
@@ -1,7 +1,6 @@
 // define all the constants that we will use in the application.
 
 package constants
-
 const ConnectionString ="mongodb+srv://user:pass@cluster0.mom8ir1.mongodb.net/"
 const Port =":4000"
 const DatabaseName ="Poultry"
