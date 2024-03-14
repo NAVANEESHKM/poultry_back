30fe0e8d94513909c6e8da8323fb80bcdfc2f272
NAVANEESH K M <navaneesh.ee20@bitsathy.ac.in>
2024-03-14 17:04:17 +0530

    u

--- constants/index.go
+++ constants/index.go
@@ -3,9 +3,7 @@
 package constants
 
 const ConnectionString ="mongodb+srv://user:pass@cluster0.mom8ir1.mongodb.net/"
-
-//mongodb+srv://navaneeshmuthusamy:Neesh04%4012%402002@cluster0.j5hnmx6.mongodb.net/??retryWrites=true&w=majority
 const Port =":4000"
 const DatabaseName ="Poultry"
 //"mongodb+srv://userone:L2ORM855sl5XzunU@cluster0.9eqsnyg.mongodb.net/"
-                           
+            
