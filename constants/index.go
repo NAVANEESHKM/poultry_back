10e1fc38abf69bc619b0e30925a68e74c5c71a96
NAVANEESHKM <navaneeshmuthusamy@gmail.com>
2024-03-28 15:11:07 +0530

    constant file change

--- constants/index.go
+++ constants/index.go
@@ -1,5 +1,5 @@
 package constants
 const ConnectionString ="mongodb+srv://user:pass@cluster0.mom8ir1.mongodb.net/"
-const Port =":4000"
+const Port =":4044"
 const DatabaseName ="Poultry"
             
