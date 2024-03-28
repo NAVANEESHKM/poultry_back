f2ca7938f4524355e9ecd0d2c900092aa4ce5265
NAVANEESHKM <navaneeshmuthusamy@gmail.com>
2024-03-28 14:55:55 +0530

    constant file change

--- constants/index.go
+++ constants/index.go
@@ -1,8 +1,5 @@
-// define all the constants that we will use in the application.
-
 package constants
 const ConnectionString ="mongodb+srv://user:pass@cluster0.mom8ir1.mongodb.net/"
 const Port =":4000"
 const DatabaseName ="Poultry"
-//"mongodb+srv://userone:L2ORM855sl5XzunU@cluster0.9eqsnyg.mongodb.net/"
             
